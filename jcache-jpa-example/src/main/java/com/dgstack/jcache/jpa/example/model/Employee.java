d4f7953c91a8318a75ecae12300d4a4022e45d5c
Digvijay Bhakuni <digvijay.bhakuni@rediffmail.com>
2017-06-11 21:32:27 +0530

    init for jcache jpa example

---
+++ jcache-jpa-example/src/main/java/com/dgstack/jcache/jpa/example/model/Employee.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dgstack.jcache.jpa.example.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author digvijayb
+ */
+@Entity
+public class Employee {
+    private Long id;
+    private String name;
+    private String department;
+   
+    
+    
+}
