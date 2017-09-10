782a41c572be70f794870f1a2d8c45632c8eb005
Digvijay Bhakuni <digvijay.bhakuni@rediffmail.com>
2017-09-10 23:48:42 +0530

    sping boot micro-services setup 1

---
+++ mircoservices/demo/src/main/java/com/example/web/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("demo")
+	public String index(){
+		return "index";
+	}
+
+}
