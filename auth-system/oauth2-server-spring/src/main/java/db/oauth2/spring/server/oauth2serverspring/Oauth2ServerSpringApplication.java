72811dd0ae5c345a34f730fc76fc059333880b1d
digivjayb.kovid <dbhakuni@KOVIDDT033.KovidGroup.com>
2017-10-11 21:56:38 +0530

    auth mod

--- auth-system/oauth2-server-spring/src/main/java/db/oauth2/spring/server/oauth2serverspring/Oauth2ServerSpringApplication.java
+++ auth-system/oauth2-server-spring/src/main/java/db/oauth2/spring/server/oauth2serverspring/Oauth2ServerSpringApplication.java
@@ -3,6 +3,7 @@ package db.oauth2.spring.server.oauth2serverspring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -10,6 +11,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -19,36 +22,50 @@ import java.security.Principal;
 @SpringBootApplication
 @EnableResourceServer
 @RestController
-public class Oauth2ServerSpringApplication extends WebMvcConfigurerAdapter{
+public class Oauth2ServerSpringApplication extends WebMvcConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Oauth2ServerSpringApplication.class, args);
-	}
-
-
-
-	@Configuration
-	@EnableAuthorizationServer
-	protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
-		@Autowired
-		private AuthenticationManager authenticationManager;
-
-		@Override
-		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-			endpoints.authenticationManager(authenticationManager);
-		}
-
-		@Override
-		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-			clients.inMemory().withClient("foo").secret("foosecret")
-					.authorizedGrantTypes("authorization_code", "refresh_token", "password").scopes("openid");
-		}
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ServerSpringApplication.class, args);
+    }
 
 
-	@RequestMapping("/user")
-	public Principal user(Principal user) {
-		System.out.println("user  = " + user);
-		return user;
-	}
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Bean
+        public JwtAccessTokenConverter accessTokenConverter() {
+            JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+
+            return converter;
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints
+                    .authenticationManager(authenticationManager)
+                    .accessTokenConverter(accessTokenConverter());
+        }
+
+//        @Override
+//        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+//            oauthServer.tokenKeyAccess("isAnonymous() || hasAuthority('ROLE_TRUSTED_CLIENT')")
+//                    .checkTokenAccess("hasAuthority('ROLE_TRUSTED_CLIENT')");
+//        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory().withClient("foo").secret("foosecret")
+                    .authorizedGrantTypes("authorization_code", "refresh_token", "password").scopes("openid");
+        }
+    }
+
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        System.out.println("user  = " + user);
+        return user;
+    }
 }
